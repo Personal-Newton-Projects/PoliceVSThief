7dbabcc2916821e04a8292214abdf0205546e56a
Olivier Rataj <olivier.rataj@edu.newton.se>
2021-10-12 23:11:25 +0200

    Update Main() with new comments

--- PoliceVSThief/Program.cs
+++ PoliceVSThief/Program.cs
@@ -4,17 +4,16 @@ using System.Collections.Generic;
 namespace PoliceVSThief
 {
     class Program
-    {
-        
+    {   
         static void Main()
         {
             
-            GameManager.GeneratePeople(20, 10, 5);
+            GameManager.GeneratePeople(20, 10, 5); // Generate 20 Citizen, 10 Thieves, 5 Police
             while (true)
             {
                 Console.CursorVisible = false;       // Keep the cursor invisible
-                GameManager.MoveAllPeople();         // Keep everyone movin'!
-                System.Threading.Thread.Sleep(63);  // Don't goo too fast.
+                GameManager.MoveAllPeople();         // Keep everyone moving!
+                System.Threading.Thread.Sleep(63);   // Don't goo too fast.
                 GameManager.FlipEveryone();          // Randomize the direction every iteration
                 GameManager.RemoveEmptyCitizen();    // Remove all empty Citizen!
                 LogBox.LogPeopleCount();             // Log the amount of people
